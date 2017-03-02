35c6d158ae4d3ddd1442dfb57ee37649f160c0d4
Rufus Cable <rufus@threebytesfull.com>
2017-03-02 14:45:54 +0000

    Tidy up imports

--- tests/test_messages.py
+++ tests/test_messages.py
@@ -1,4 +1,7 @@
-import unittest, unittest.mock, io
+import io
+
+import unittest
+from unittest.mock import patch
 
 from pcbmode.utils import messages as msg
 
@@ -7,54 +10,54 @@ class TestMessages(unittest.TestCase):
 
     # info tests
     def test_info_with_default_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.info('some information')
             self.assertEqual(fake_out.getvalue(), '-- some information\n')
 
     def test_info_with_specified_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.info('some information', newline=True)
             self.assertEqual(fake_out.getvalue(), '-- some information\n')
 
     def test_info_with_no_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.info('some information', newline=False)
             self.assertEqual(fake_out.getvalue(), '-- some information')
 
     # note tests
     def test_note_with_default_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.note('a note')
             self.assertEqual(fake_out.getvalue(), '-- NOTE: a note\n')
 
     def test_note_with_specified_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.note('a note', newline=True)
             self.assertEqual(fake_out.getvalue(), '-- NOTE: a note\n')
 
     def test_note_with_no_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.note('a note', newline=False)
             self.assertEqual(fake_out.getvalue(), '-- NOTE: a note')
 
     # subInfo tests
     def test_sub_info_with_default_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.subInfo('some sub-information')
             self.assertEqual(fake_out.getvalue(), ' * some sub-information\n')
 
     def test_sub_info_with_specified_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.subInfo('some sub-information', newline=True)
             self.assertEqual(fake_out.getvalue(), ' * some sub-information\n')
 
     def test_sub_info_with_no_newline(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             msg.subInfo('some sub-information', newline=False)
             self.assertEqual(fake_out.getvalue(), ' * some sub-information')
 
     def test_error_without_type(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             with self.assertRaises(Exception, msg='error should raise Exception'):
                 msg.error('error details')
             self.assertEqual(fake_out.getvalue(),
@@ -65,7 +68,7 @@ class TestMessages(unittest.TestCase):
                     '-----------------------------\n')
 
     def test_error_with_type(self):
-        with unittest.mock.patch('sys.stdout', new=io.StringIO()) as fake_out:
+        with patch('sys.stdout', new=io.StringIO()) as fake_out:
             with self.assertRaises(TypeError, msg='error should raise error of specified type'):
                 msg.error('error details', error_type=TypeError)
             self.assertEqual(fake_out.getvalue(),
