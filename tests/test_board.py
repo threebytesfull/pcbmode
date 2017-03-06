bcba1907892aa28a52bbc184c37a373714f48641
Rufus Cable <rufus@threebytesfull.com>
2017-03-06 12:40:04 +0000

    Add tests for Board object

--- tests/test_board.py
+++ tests/test_board.py
@@ -2,9 +2,23 @@ import unittest
 from unittest.mock import patch
 
 from pcbmode.utils.board import Board
+from pcbmode.config import Config
 
 class TestBoard(unittest.TestCase):
     """Test pcbmode script"""
 
-    def test_board(self):
-        pass
+    @patch('pcbmode.utils.board.Module')
+    def test_board_instantiates_module(self, mock_module):
+        """
+        When a Board object is created, it instantiates a Module object. It
+        looks like it doesn't actually need to store references to the config
+        as they're not used anywhere else. These can probably be removed...
+        """
+        board = Board()
+        self.assertTrue(mock_module.called, 'Board should instantiate a Module')
+        self.assertTrue(hasattr(board, '_module_dict'), 'board should have protected module_dict attribute')
+        self.assertTrue(hasattr(board, '_module_routing'), 'board should have protected module_routing attribute')
+
+        c = Config()
+        self.assertIs(board._module_dict, c.get('brd'), 'module_dict attribute should be same object as config.brd')
+        self.assertIs(board._module_routing, c.get('rte'), 'module_routing attribute should be same object as config.rte')
