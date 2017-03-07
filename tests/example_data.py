44b81be305f9743cacad70af80a21beec3a7de90
Rufus Cable <rufus@threebytesfull.com>
2017-03-07 17:17:56 +0000

    Add simple class for test fixture data

---
+++ tests/example_data.py
@@ -0,0 +1,19 @@
+import os
+import pkg_resources
+
+try:
+    from pathlib import Path
+except ImportError:
+    from pathlib2 import Path
+
+class ExampleData(object):
+    def __init__(self, *path_components):
+        self.path_components = path_components
+
+    @property
+    def path(self):
+        return pkg_resources.resource_filename(__name__, os.path.join('data', *self.path_components))
+
+    @property
+    def text_content(self, encoding=None):
+        return Path(self.path).read_text(encoding=encoding)
---
+++ tests/test_example_data.py
@@ -0,0 +1,25 @@
+import unittest
+
+from tests.example_data import ExampleData
+
+class TestExampleData(unittest.TestCase):
+    """Test ExampleData class"""
+
+    def test_instantiate_with_valid_single_level_path(self):
+        d = ExampleData('simple.txt')
+        self.assertIsNotNone(d, 'should get an example data object')
+        self.assertRegex(d.path, r'\bsimple\.txt$', 'should get a path with supplied filename')
+
+    def test_instantiate_with_valid_multi_level_path(self):
+        d = ExampleData('stuff', 'simple.txt')
+        self.assertIsNotNone(d, 'should get an example data object')
+        self.assertRegex(d.path, r'\bsimple\.txt$', 'should get a path with supplied filename components')
+
+    def test_content_with_valid_file(self):
+        d = ExampleData('example.txt')
+        self.assertEqual(d.text_content, '# Example Data\n', 'should get fixture file contents')
+
+    def test_content_with_invalid_file(self):
+        d = ExampleData('no_such_file.txt')
+        with self.assertRaises(IOError, msg='should raise exception reading nonexistent file'):
+            d.text_content()
